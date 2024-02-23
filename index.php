7b94867e8473d2fc2342cf1effbb2be39a6507f6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-23 18:32:24 +0100

    added method __costruct

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ class Movie {
     public $genre;
     public $duration;
     public $original_language;
+
+    public function __construct($title, $year_of_publication, $genre, $duration, $original_languag) {
+        $this->$title = $title;
+        $this->$year_of_publication = $year_of_publication;
+        $this->$genre = $genre;
+        $this->$duration = $duration;
+        $this->$original_language = $original_language;
+    }
 }
 ?>
