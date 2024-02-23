7adde4f187dc52c76471f9709a47a30fbf469cf7
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-23 18:39:58 +0100

    fixed __construct

--- index.php
+++ index.php
@@ -7,14 +7,13 @@ class Movie {
     public $original_language;
 
     public function __construct($title, $year_of_publication, $genre, $duration, $original_languag) {
-        $this->$title = $title;
-        $this->$year_of_publication = $year_of_publication;
-        $this->$genre = $genre;
-        $this->$duration = $duration;
-        $this->$original_language = $original_language;
+        $this->title = $title;
+        $this->year_of_publication = $year_of_publication;
+        $this->genre = $genre;
+        $this->duration = $duration;
+        $this->original_language = $original_language;
     }
 }
 
 $movie = new Movie('Toy Storu', 1995, ['animazione', 'commedia'], 81, 'EN');
-
 ?>
