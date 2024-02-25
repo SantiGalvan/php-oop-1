e94f3f323d5aa0bc928605775479a1cf675ddbb2
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-25 21:50:09 +0100

    added actor into movie

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ class Movie {
     public $hour;
     public $minutes;
     public $director;
+    public $actor;
 
-    public function __construct($title, $year_of_publication, $genre, $original_language, $duration, Director $director) {
+    public function __construct($title, $year_of_publication, $genre, $original_language, $duration, Director $director, Actor $actor) {
         $this->title = $title;
         $this->year_of_publication = $year_of_publication;
         $this->genre = $genre;
         $this->original_language = $original_language;
         $this->duration = $duration;
         $this->director = $director;
+        $this->actor = $actor;
         $this->setDuration();
     }
 
@@ -49,18 +51,25 @@ class Actor {
     public $last_name;
     public $age;
     public $nationality;
+
+    public function __construct($name, $last_name, $age, $nationality) {
+        $this->name = $name; 
+        $this->last_name = $last_name;
+        $this->age = $age; 
+        $this->nationality = $nationality; 
+    }
 }
 
 $director_movie = new Director('John', 'Lasseter', 67, 'USA');
-$movie = new Movie('Toy Story', 1995, ['animazione', 'commedia'], 'EN', 81, $director_movie);
+$actor_movie = new Actor('Woody', 'Pride', 29, 'USA');
+$movie = new Movie('Toy Story', 1995, ['animazione', 'commedia'], 'EN', 81, $director_movie, $actor_movie);
 
 var_dump($movie);
-var_dump($director_movie);
 
 $director_second_movie = new Director('Sean', 'Penn', 64, 'USA');
-$second_movie = new Movie('Into the Wild', 2007, ['avventura', 'biografico', 'drammatico'], 'EN', 148, $director_second_movie);
+$actor_second_movie = new Actor('Emile', 'Hirsch', 39, 'USA');
+$second_movie = new Movie('Into the Wild', 2007, ['avventura', 'biografico', 'drammatico'], 'EN', 148, $director_second_movie, $actor_second_movie);
 
 var_dump($second_movie);
-var_dump($director_second_movie);
 
 ?>
